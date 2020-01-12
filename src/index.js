e721d4e445bd90bf272c906e47eedac4b4329623
Eric Reale <ecr5083@gmail.com>
2020-01-12 16:07:24 -0500

    front end containers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-console.log('here');
+const cards = Array.from(document.querySelectorAll('.card-trip'));
 
+function handleMouseOver(e) {
+  // console.log(e)
+    const cardSelected = e.currentTarget;
+    const imgOnCard = cardSelected.querySelector('img');
+    cardSelected.classList.add('hover');
+    imgOnCard.classList.add('hover-img')
+}
+
+function removeTransition(e) {
+    // console.log(e);
+    this.classList.remove('hover');
+    const imgOnCard = this.querySelector('img');
+    imgOnCard.classList.remove('hover-img');
+  }
+
+cards.forEach(card => {
+  card.addEventListener('mouseover', handleMouseOver);
+})
+
+cards.forEach(card => {
+  card.addEventListener('mouseout', removeTransition);
+})
 
