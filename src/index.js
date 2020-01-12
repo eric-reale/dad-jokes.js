1b65b978112fe058a24e27853b5cc0741f7cdc10
Eric Reale <ecr5083@gmail.com>
2020-01-12 16:28:07 -0500

    added js card selection

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const cards = Array.from(document.querySelectorAll('.card-trip'));
+const jokeParagraph = document.querySelector('#joke-paragraph');
 
 function handleMouseOver(e) {
   // console.log(e)
@@ -15,8 +16,34 @@ function removeTransition(e) {
     imgOnCard.classList.remove('hover-img');
   }
 
+async function fetchDadJoke() {
+  const response = await fetch('https://icanhazdadjoke.com', {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  const data = await response.json();
+  return data;
+}
+
+async function handleClick(e) {
+  switch (e.currentTarget.textContent.trim()) {
+    case "Dad Jokes": console.log("dad jokes");
+      break;
+    default: console.log('No click matches');
+  }
+
+
+
+  // const { joke } = await fetchDadJoke();
+  // console.log(joke);
+  // jokeHolder.textContent = joke;
+  // jokeButton.textContent = randomItemFromArray(buttonText, jokeButton.textContent); // here's where you're passing in the previous one
+}
+
 cards.forEach(card => {
   card.addEventListener('mouseover', handleMouseOver);
+  card.addEventListener('click', handleClick);
 })
 
 cards.forEach(card => {
